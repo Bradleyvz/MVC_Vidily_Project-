4e1af4cd08765ac087e679c0d9cd0226a36d8239
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 22:27:30 +0200

    Section 2 Commit Not working

--- MVC_Vidily/Controllers/MoviesController.cs
+++ MVC_Vidily/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using MVC_Vidily.Models;
+﻿using Microsoft.Owin.Security.Provider;
+using MVC_Vidily.Models;
 using MVC_Vidily.ViewModel;
 //Added in the using models using statement.
 using System;
@@ -8,56 +9,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using MVC_Vidily.ViewModel;
 
 namespace MVC_Vidily.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        //Create a List Of Customers
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie> 
+            { 
+            new Movie{Id=1,Name="Sherk"},
+            new Movie{Id=2,Name="Wall-e"}
+            };
+
+        }
+        //GET:Movies/Random
         public ActionResult Random()
         {
-
-            var movie = new Movie() { Name = "Sherk" };
+            var movies = new Movie() { Name = "Sherk" };
             var customers = new List<Customer>
-            {
-                new Customer{Name="Customer1"},
-                new Customer{Name="Customer2"}
+            
+            { 
+            new Customer{Name="Customer1"},
+            new Customer{Name="Customer2"}
 
             };
             var viewModel = new RandomViewModel
             {
-                Movie = movie,
-                Customers=customers
+                Movie = movies,
+                Customers = customers
+
             };
-
             return View(viewModel);
-        }
-    
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            
-                pageIndex = 1;
-                if (string.IsNullOrWhiteSpace(sortBy))
-                
-                    sortBy = "Name";
-
-
-                    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        }
-        //Controller Action for the Custom Route in Route.Config File 
-        //Adding the Atrribute Routing in the Controller.
-        [Route("movies.released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month )
-        {
-
-            return Content(year +"/"+month);
+       
         }
 
     }
