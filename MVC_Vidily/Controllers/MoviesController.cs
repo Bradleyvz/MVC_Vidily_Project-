5156a4afe05b98fb47b1aac1622125574d0597e6
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 19:33:41 +0200

    Added Model and Random Controller

---
+++ MVC_Vidily/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_Vidily.Models;
+//Added in the using models using statement.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Vidily.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie=new Movie() { }
+            return View();
+        }
+    }
+}
---
+++ MVC_Vidily/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace MVC_Vidily.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
