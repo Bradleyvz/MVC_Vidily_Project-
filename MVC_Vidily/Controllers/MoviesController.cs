bb6fd4b88622f68c3d9b5661fc39b7c970e8a77a
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 20:10:26 +0200

    Paramters in the URL

--- MVC_Vidily/Controllers/MoviesController.cs
+++ MVC_Vidily/Controllers/MoviesController.cs
@@ -16,5 +16,26 @@ namespace MVC_Vidily.Controllers
             var movie = new Movie() { Name = "Sherk" };
             return View(movie);
         }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            
+                pageIndex = 1;
+                if (string.IsNullOrWhiteSpace(sortBy))
+                
+                    sortBy = "Name";
+
+
+                    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
+
     }
 }
+    
+
+
