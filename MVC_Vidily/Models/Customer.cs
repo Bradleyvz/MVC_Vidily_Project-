4f06235584767df0a47afe6cbe7658639a073151
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-13 22:23:48 +0200

    Added Data Anotations

--- MVC_Vidily/Models/Customer.cs
+++ MVC_Vidily/Models/Customer.cs
@@ -11,6 +11,8 @@ namespace MVC_Vidily.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         
