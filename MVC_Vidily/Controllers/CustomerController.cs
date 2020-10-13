47915ebdb45c99969df18379c649d91a84d9a74b
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-13 22:41:46 +0200

    Dispplay Cumstomers from Sql Database Last Commit

--- MVC_Vidily/Controllers/CustomerController.cs
+++ MVC_Vidily/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC_Vidily.Models;
 using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,31 +10,33 @@ namespace MVC_Vidily.Controllers
 {
     public class CustomerController : Controller
     {
+        private DataContext _context;
+        public CustomerController()
+        {
+            _context = new DataContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-        {
-            new Customer { Id = 1, Name = "John Smith" },
-            new Customer { Id = 2, Name = "Mary Williams" }
-        };
         }
     }
 }
