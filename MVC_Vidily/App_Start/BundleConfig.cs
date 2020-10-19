ff72bfb3aac6f17103dd606bdef205c91d411ad6
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-19 19:31:06 +0200

    Add BootBox  Jquery Added Ajax Delete

--- MVC_Vidily/App_Start/BundleConfig.cs
+++ MVC_Vidily/App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace MVC_Vidily
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Script/respond.js",
+                      "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
